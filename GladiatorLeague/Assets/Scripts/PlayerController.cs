8b572bcd4312ef715089f96878f6887164f56d77
dyzalonius <dyzalonius@gmail.com>
2019-01-25 18:12:40 +0100

    Added basic networking. Added player motor, controls and setup.

---
+++ GladiatorLeague/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : NetworkBehaviour
+{
+    private PlayerMotor playerMotor;
+
+    private void Start()
+    {
+        playerMotor = GetComponentInParent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Get movement input
+        Vector3 direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            direction.z++;
+        if (Input.GetKey(KeyCode.A))
+            direction.x--;
+        if (Input.GetKey(KeyCode.S))
+            direction.z--;
+        if (Input.GetKey(KeyCode.D))
+            direction.x++;
+
+        // Update direction if it's new
+        if (playerMotor.direction != direction)
+        {
+            playerMotor.direction = direction;
+        }
+    }
+}
---
+++ GladiatorLeague/Assets/Scripts/SceneCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneCamera : MonoBehaviour
+{
+    [SerializeField] private Behaviour[] componentsToDisable;
+
+    public void DisableComponents()
+    {
+        foreach (Behaviour component in componentsToDisable)
+        {
+            component.enabled = false;
+        }
+    }
+
+    public void EnableComponents()
+    {
+        foreach (Behaviour component in componentsToDisable)
+        {
+            component.enabled = true;
+        }
+    }
+}
